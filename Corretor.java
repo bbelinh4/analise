082d449b7dde361940e4c51aad58260762b9c2c4
bbelinh4 <145396281+bbelinh4@users.noreply.github.com>
2023-09-19 22:48:40 -0300

    Create Corretor.java

---
+++ Corretor.java
@@ -0,0 +1,13 @@
+package Domain;
+
+import java.util.List;
+
+public class Corretor {
+
+    private Integer cod_corretor;
+    private String telefone;
+    private String nome_corretor;
+    private String sexo;
+
+    private List<ContratoCompra>contratos;
+}
